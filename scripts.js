f8da43f86e855f0fe2b5b5ddca6495ca4b7b563f
J.C. Hiatt <jc@jchiatt.com>
2018-11-20 09:02:16 -0600

    add functions to play/pause and to update the play/pause icon

---
+++ scripts.js
@@ -0,0 +1,24 @@
+/* Get all the elemnts */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Player funcftions */
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+/* Event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
